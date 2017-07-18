e7d9e33ac1f8dd4422051f28a36adfcaa8103f8b
Andrew Stout <andrew@andrewstout.me>
2017-07-18 15:16:38 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const Hapi = require('hapi');
+const laabr = require('laabr');
+
+const server = new Hapi.Server();
+server.connection({ port: 3002, host: 'localhost' });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.start((err) => {
+
+    laabr.format('response', ':cid - :req[x-request-id] - :time[utc], :tags :req[host] :method :url :status (:responseTime)');
+
+    server.register({
+      register: laabr.plugin,
+      options: {
+        colored: true,
+        correlator: {
+          enabled: true,
+          header: 'x-request-id'
+        },
+        pino: {
+          level: 'trace'
+        },
+        hapiPino: {
+          logPayload: true,
+          mergeHapiLogData: true
+        }
+      }
+    });
+
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+});
